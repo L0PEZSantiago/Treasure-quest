d37bc745bc8a6098aa977e97c39c14b034224c63
Santiago <santi@atrevise.com>
2025-02-24 18:29:01 +0100

    Placement du dragon aléatoire terminé

--- script.js
+++ script.js
@@ -1,104 +1,114 @@
-    document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-        // Ci-dessous le script JS pour gérer la génération de la grille et le placement du coffre, des rochers et du trésor.
-        let grid = document.querySelector('.grid');
-        let existingIndex = [];
-        // Générer une grille de 20x20
-        for (let i = 0; i < 400; i++) {
-            let cell = document.createElement('div');
-            cell.classList.add('cell');
-            grid.appendChild(cell);
-        }
-        const cellArray = Array.from(document.querySelectorAll('.cell'));
+    // Ci-dessous le script JS pour gérer la génération de la grille et le placement du coffre, des rochers et du trésor.
+    let grid = document.querySelector('.grid');
+    let existingIndex = [];
+    // Générer une grille de 20x20
+    for (let i = 0; i < 400; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
+    const cellArray = Array.from(document.querySelectorAll('.cell'));
 
-        function success() {
-            grid.classList.toggle('success')
-            grid.innerHTML = `<h2>Bravo, vous avez trouvé le trésor!</h2>
+    function success() {
+        grid.classList.toggle('success')
+        grid.innerHTML = `<h2>Bravo, vous avez trouvé le trésor!</h2>
                               <button class="btn-replay">Rejouer!</button>`;
 
-            const btn = document.querySelector('.btn-replay');
-            btn.addEventListener('click', () => {
-                location.reload();
-                document.querySelector('.grid').classList.remove('.success');
-            });
+        const btn = document.querySelector('.btn-replay');
+        btn.addEventListener('click', () => {
+            location.reload();
+            document.querySelector('.grid').classList.remove('.success');
+        });
+    }
+
+    // Ajouter le player
+    playerIndex = cellArray[0];
+    playerIndex.classList.add('player');
+    existingIndex.push(playerIndex);
+
+
+    // Ajouter le trésor aléatoirement
+    let treasureIndex = Math.floor(Math.random() * cellArray.length);
+    cellArray[treasureIndex].classList.add('treasure');
+    existingIndex.push(treasureIndex);
+
+
+    // Ajouter le dragon
+    // Le dragon ne doit pas se trouver sur le trésor
+
+
+    let dragonIndex = treasureIndex;
+    while (dragonIndex == treasureIndex) {
+        dragonIndex = Math.floor(Math.random() * cellArray.length);
+    }
+    cellArray[dragonIndex].classList.add('blue-dragon');
+    existingIndex.push(dragonIndex);
+
+    console.log(dragonIndex + ' ' + treasureIndex);
+
+
+
+    // Création d'une boucle pour ajouter des rochers aléatoirement
+    let count = 0;
+    while (count != 50) {
+        let rockIndex = Math.floor(Math.random() * cellArray.length);
+
+        if (!existingIndex.includes(rockIndex) && rockIndex != 0) {
+            cellArray[rockIndex].classList.add('rock');
+            existingIndex.push(rockIndex);
+            count++;
         }
+    }
 
-        // Ajouter des rochers aléatoirement
-        // Ajouter le trésor aléatoirement
-        let treasureIndex = Math.floor(Math.random() * cellArray.length);
-        cellArray[treasureIndex].classList.add('treasure');
-        existingIndex.push(treasureIndex);
-
-
-        let dragonIndex = Math.floor(Math.random() * cellArray.length);
-        cellArray[dragonIndex].classList.add('blue-dragon');
-        existingIndex.push(dragonIndex);
-
-
-
-        // Ajouter le player
-        playerIndex = cellArray[0];
-        playerIndex.classList.add('player');
-        existingIndex.push(playerIndex);
-        // Création d'une boucle pour ajouter des rochers aléatoirement
-        let count = 0;
-        while (count != 50) {
-            let rockIndex = Math.floor(Math.random() * cellArray.length);
-
-            if (!existingIndex.includes(rockIndex) && rockIndex != 0) {
-                cellArray[rockIndex].classList.add('rock');
-                existingIndex.push(rockIndex);
-                count++;
-            }
-        }
-        
-        // Fin boucle treasure
-        // Faire fonctionner le déplacement du joueur avec les touches directionnelles.
-        let player = document.querySelector('.player');
-        playerIndex = cellArray.indexOf(player);
-        document.addEventListener('keydown', (event) => {
-            let key = event.key;
-            let newPlayerIndex;
-            switch (key) {
-                case 'ArrowUp':
-                    newPlayerIndex = playerIndex - 20;
-                    break;
-                case 'ArrowDown':
-                    newPlayerIndex = playerIndex + 20;
-                    break;
-                case 'ArrowLeft':
-                    if (playerIndex % 20 != 0) {
-                        newPlayerIndex = playerIndex - 1;
-                    }
-                    break;
-                case 'ArrowRight':
-                    if (playerIndex % 20 != 19) {
-                        newPlayerIndex = playerIndex + 1;
-                    }
-                    break;
-                default:
-                    return;
-            }
-            if (cellArray[newPlayerIndex].classList.contains('rock')) {
+    // Fin boucle treasure
+    // Faire fonctionner le déplacement du joueur avec les touches directionnelles.
+    let player = document.querySelector('.player');
+    playerIndex = cellArray.indexOf(player);
+    document.addEventListener('keydown', (event) => {
+        let key = event.key;
+        let newPlayerIndex;
+        switch (key) {
+            case 'ArrowUp':
+                newPlayerIndex = playerIndex - 20;
+                break;
+            case 'ArrowDown':
+                newPlayerIndex = playerIndex + 20;
+                break;
+            case 'ArrowLeft':
+                if (playerIndex % 20 != 0) {
+                    newPlayerIndex = playerIndex - 1;
+                }
+                break;
+            case 'ArrowRight':
+                if (playerIndex % 20 != 19) {
+                    newPlayerIndex = playerIndex + 1;
+                }
+                break;
+            default:
                 return;
-            }
+        }
+        if (cellArray[newPlayerIndex].classList.contains('rock')) {
+            return;
+        }
 
-            cellArray[playerIndex].classList.remove('player');
-            cellArray[newPlayerIndex].classList.add('player');
-            playerIndex = newPlayerIndex;
-
-
-
-            if (newPlayerIndex == treasureIndex) {
-                success();
-            }
-
-        }); // Fin de la gestion des touches
+        cellArray[playerIndex].classList.remove('player');
+        cellArray[newPlayerIndex].classList.add('player');
+        playerIndex = newPlayerIndex;
 
 
 
+        if (newPlayerIndex == treasureIndex) {
+            success();
+        }
 
-    });
+    }); // Fin de la gestion des touches
+
+
+
+
+});
 
 
 
