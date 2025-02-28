9220fbf598c68ee0d79d6f7a94244824f65c9fad
Santiago Lopez <santi@atrevise.com>
2025-02-28 02:26:41 +0100

    update: version mobile adaptée, grid, spawn etc

--- script.js
+++ script.js
@@ -15,19 +15,29 @@ const myAudio = document.querySelector('audio');
     myAudio.preload = 'auto';
 })();
 
-
-
+/* ----------------- VARIABLES DOM ----------------- */
+const checksboxs = document.querySelectorAll('input[type="checkbox"]');
 const btnUp = document.querySelector('.btnUp');
 const btnDown = document.querySelector('.btnDown');
 const btnLeft = document.querySelector('.btnLeft');
 const btnRight = document.querySelector('.btnRight');
 const grid = document.querySelector('.grid');
-
 let btnStart = document.querySelector('.btn-set-difficulty');
-console.log(btnStart);
 
-
+/* ----------------- VARIABLES GLOBALES ----------------- */
 let difficultyMax = 14;
+
+let isMobile = localStorage.getItem('isMobile');
+console.log(isMobile == 'true');
+let nbrRows;
+if (isMobile == 'true') {
+    nbrRows = 10;
+} else {
+    nbrRows = 20;
+}
+
+let nbrColumns = nbrRows;
+console.log(nbrRows, "nbrRows", "nbrColumns", nbrColumns);
 const dragons = [];
 const portals = [];
 const bluePortalsIndexs = [];
@@ -38,23 +48,59 @@ let countTreasure = 0;
 let totalForWin;
 const treasuresDiv = document.querySelector('.treasures');
 
+/* ----------------- LISTENER SUR LES CHECKBOXS ----------------- */
+let checkboxTarget;
+checksboxs.forEach(checkbox => {
+    checkbox.addEventListener('change', e => {
+        if (e.currentTarget.checked) {
+            // Parcourt tous les checkboxes et décoche ceux qui ne sont pas le currentTarget
+            checksboxs.forEach(box => {
+                if (box !== e.currentTarget) {
+                    box.checked = false;
+                }
+            });
+            checkboxTarget = e.currentTarget;
+            console.log(checkboxTarget);
+        }
+    });
+});
+
+
+
+/* ----------------- LISTENER SUR LE BOUTON START ----------------- */
+
 btnStart.addEventListener('click', () => {
     const input = document.querySelector('input');
     totalForWin = input.value;
+    if (checkboxTarget == undefined || input.value == '') {
+        alert('Veuillez entrer les paramètres requis');
+        return;
+    } else if (checkboxTarget.id == "Mobile") {
+        isMobile = true;
+    } else if (checkboxTarget.id == "Ordinateur") {
+        isMobile = false;
+    }
+    // Soit je fais if checkboxTarget(à vérifier), soit je fais une boolean isChecked
     if (input.value > 20) {
         input.value = 20;
         alert('Veuillez choisir une valeur comprise entre 1 et 14');
         console.log("click", totalForWin);
         location.reload();
-        return totalForWin;
+    } else {
+        localStorage.setItem('totalForWin', totalForWin);
+        localStorage.setItem('isMobile', isMobile);
+        document.querySelector('.difficulty').style.display = 'none';
+        checkboxTarget.checked = false;
+        location.reload();
     }
-    localStorage.setItem('totalForWin', totalForWin);
-    document.querySelector('.difficulty').style.display = 'none';
-    location.reload();
 })
 
-// Je dois faire en sorte que quand j'ai choisi la quantité, la page se recharge et le jeu commence
 
+/* ----------------- INITIALISATION ----------------- */
+
+// Je dois faire en sorte que quand j'ai choisi la quantité, la page se recharge et le jeu commence
+// Si j'ai choisi une difficulté, elle sera forcémment dans le local storage car je la stocke au chargement de la page
+// Comme ça lorsque je clique sur GO, cela recharge la page mais je reprends la difficulté choisie avant le rechargement
 if (localStorage.getItem('totalForWin')) {
     totalForWin = localStorage.getItem('totalForWin');
     document.querySelector('.difficulty').style.display = 'none';
@@ -62,6 +108,7 @@ if (localStorage.getItem('totalForWin')) {
     localStorage.removeItem('totalForWin');
 }
 
+// QUANTITE DE TREASURE
 for (let i = 0; i < totalForWin; i++) {
     treasuresDiv.innerHTML += `<img src="assets/treasure.png" alt="treasure">
 `;
@@ -70,9 +117,11 @@ for (let i = 0; i < totalForWin; i++) {
 // QUANTITE DE VIE
 let life;
 
+// Gestion des vies selons la quantité de treasures choisie/dfficulté + stockage du max de vies du début
 totalForWin <= 5 ? life = 3 : totalForWin > 5 && totalForWin <= 8 ? life = 4 : life = 5;
 let maxLife = life;
 
+// Affichage des vies selon la quantité de vies choisie
 for (let i = 0; i < life; i++) {
     document.querySelector('.lifes').innerHTML += `<i class="fa-solid fa-heart"></i>
 `;
@@ -80,8 +129,8 @@ for (let i = 0; i < life; i++) {
 let heartArray = Array.from(document.querySelectorAll('i'));
 
 
+/* ----------------- CLASSES ----------------- */
 
-// Je fais ma classe dragon, quand je crée un dragon, il est push dans le tableau dragons ci-dessus
 class Dragon {
     constructor(index, type, className, isActive = false) {
         this.index = index;
@@ -97,27 +146,34 @@ class Portal {
         this.index = index;
         this.color = color;
         this.className = className;
+        this.row = calculateRow(this.index, nbrRows);
+        this.col = calculateCol(this.index, nbrColumns);
 
-        portals.push(this);
-        if (this.color === 'blue') {
-            bluePortalsIndexs.push(index);
-        } else {
-            purplePortalsIndexs.push(index);
-        }
+        // console.log("Class:", this.className, "Row:", this.row, "Col:", this.col);
 
-        calculateCol(this.index);
-        calculateRow(this.index);
-    } 
-
+        // Je dois calculer la col et la row de chaque portal
+        calculateCol(index, nbrColumns);
+        calculateRow(index, nbrRows);
+    }
 }
 
+/* ----------------- CREATION GRID ----------------- */
 
 // Générer une grille de 20x20
-for (let i = 0; i < 400; i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.appendChild(cell);
+if (isMobile == 'true') {
+    for (let i = 0; i < 200; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
+} else {
+    for (let i = 0; i < 400; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
 }
+
 
 // Créer un tableau contenant toutes les cellules
 const cellArray = Array.from(document.querySelectorAll('.cell'));
@@ -129,70 +185,92 @@ let playerIndex = cellArray[0];
 playerIndex.classList.add('player');
 existingIndex.push(0);
 
+
+
+/* ----------------- CREATION ET POSITIONNEMENT DES PORTALS ----------------- */
 // Je crée les violets en premier pour pouvoir les comparer avec les bleus
 let purplePortal = new Portal(getRandomIndex(), "purple", "short-portal-purple");
 let purplePortal2 = new Portal(getRandomIndex(), "purple", "short-portal-purple");
 let bluePortal = new Portal(getRandomIndex(), "blue", "short-portal-blue");
 let bluePortal2 = new Portal(getRandomIndex(), "blue", "short-portal-blue");
 
-
-
-while (purplePortal.index == 0){
-    purplePortal.index = getRandomIndex();
+if (isMobile == 'true') {
+    nbrColumns = 20; // A changer tout en haut
 };
 
-while(calculateRow(purplePortal.index) > 8 || calculateCol(purplePortal.index) > 8) {
-    console.log(calculateRow(purplePortal.index), calculateCol(purplePortal.index));
-    purplePortal.index = getRandomIndex();
+let lowerValue = Math.floor(nbrRows * 0.2);   // Ici j'obiens 30 % des lignes donc partie haut, donc ça renvoit 6 car 20*0.3.
+let higherValue = Math.floor(nbrRows * 0.6);  // Ici j'obiens 60 % des lignes donc partie bas, donc cela renvoit 13 car 20*0.6.
+console.log("nbrRows:", nbrRows, "nbrColumns:", nbrColumns);
+console.log("lowerValue:", lowerValue, "higherValue:", higherValue);
+// Ce qu'il faut simplement retenir, ce sont les résultats de ces opérations, ils s'adaptent au nombre de lignes et colonnes de notre grille.
+
+// Le portail violet ne peut dépasser une zone de 6 row/col de hauteur/largeur (en haut à gauche), donc tant que sa row ou sa col que l'on calcule avec la méthode de classe est supéreure à 6...
+// On boucle jusqu'au moment ou l'on trouve un index valide, en recréant directement le portal car à la création on lui donne un randomIndex, et on calcule sa row et col.
+while (purplePortal.row > lowerValue || purplePortal.col > lowerValue || purplePortal.index == 0) {
+    purplePortal = new Portal(getRandomIndex(), "purple", "short-portal-purple");
 }
 
-while(calculateRow(purplePortal2.index) < 13 || calculateCol(purplePortal2.index) < 13) {
-    console.log(calculateRow(purplePortal2.index), calculateCol(purplePortal2.index));
-    purplePortal2.index = getRandomIndex();
+// Tant que valeur row du portal2 est inférieur à higherValue... Donc ce portail sera sur la partie basse de la grille, ce portail sera en bas à droite. 
+// Car tant que sa col est inférieur à higherValue, on change son index donc inférieur à 13... Changer jusqu'à que la row soit supérieure à higherValue. Donc à partir de la colonne 13
+while (purplePortal2.row < higherValue || purplePortal2.col < higherValue) {
+    purplePortal2 = new Portal(getRandomIndex(), "purple", "short-portal-purple");
+} console.log(`PurplePortal -- MinRow = ${higherValue} | minCol: ${higherValue}\n, ${purplePortal2.row},  ${purplePortal2.col}`);
+
+
+// En haut à droite
+while (bluePortal.row > lowerValue || bluePortal.col < higherValue) {
+    bluePortal = new Portal(getRandomIndex(), "blue", "short-portal-blue");
+} 
+
+// En bas à gauche
+while (bluePortal2.row < higherValue || bluePortal2.col > lowerValue) {
+    bluePortal2 = new Portal(getRandomIndex(), "blue", "short-portal-blue");
+
 }
 
-while(calculateRow(bluePortal.index) > 8 || calculateCol(bluePortal.index) < 13) {
-    console.log(calculateRow(bluePortal.index), calculateCol(bluePortal.index));
-    bluePortal.index = getRandomIndex();
-}
-
-while(calculateRow(bluePortal2.index) < 13 || calculateCol(bluePortal2.index) > 8) {
-    console.log(calculateRow(bluePortal2.index), calculateCol(bluePortal2.index));
-    bluePortal2.index = getRandomIndex();
-}
+// Si tant que row > lowerValue, positionnement = En haut
+// Si tant que row < higherValue, positionnement = En bas
+// Si tant que col > lowerValue, positionnement = En gauche
+// Si tant que col < higherValue, positionnement = En droite
 
 
-console.log("Position finale:",calculateRow(purplePortal.index), calculateCol(purplePortal.index));
+portals.push(bluePortal2, bluePortal, purplePortal2, purplePortal);
 
+// Je stocke les indexs des portals bleus et violets dans des tableaux distincts pour pouvoir les comparer plus tard si jamais
+portals.forEach(portal => {
+    if (portal.color === 'blue') {
+        bluePortalsIndexs.push(portal.index);
+    } else {
+        purplePortalsIndexs.push(portal.index);
+    }
+});
 
-// if (totalForWin > 7) {
-//     portals.forEach(portalIndex => {
-//         if (portalIndex == purplePortal.index || portalIndex == purplePortal2.index) {
-//             cellArray[portalIndex].classList.add('short-portal-purple');
-//             existingIndex.push(portalIndex);
-//         } else if (portalIndex == purplePortal.index || portalIndex == bluePortal2.index) {
-//             cellArray[portalIndex].classList.add('short-portal-blue');
-//             existingIndex.push(portalIndex);
-//         }
-//     })
-// }
-if (totalForWin > 7) {
-    cellArray[purplePortal.index].classList.add('short-portal-purple');
-    existingIndex.push(purplePortal.index);
-    cellArray[purplePortal2.index].classList.add('short-portal-purple');
-    existingIndex.push(purplePortal2.index);
-    cellArray[bluePortal.index].classList.add('short-portal-blue');
-    existingIndex.push(bluePortal.index);
-    cellArray[bluePortal2.index].classList.add('short-portal-blue');
-    existingIndex.push(bluePortal2.index);
+// J'ajoute des portails en fonction de la difficulté
+if (totalForWin > 8) {
+    portals.forEach(portal => {
+        console.log(portal);
+        if (portal.color == "purple") {
+            cellArray[portal.index].classList.add('short-portal-purple');
+            existingIndex.push(portal.index);
+        } else {
+            cellArray[portal.index].classList.add('short-portal-blue');
+            existingIndex.push(portal.index);
+        }
+    })
 } else {
-    cellArray[purplePortal.index].classList.add('short-portal-purple');
-    existingIndex.push(purplePortal.index);
-    cellArray[purplePortal2.index].classList.add('short-portal-purple');
-    existingIndex.push(purplePortal2.index);
+    portals.forEach(portal => {
+        if (portal.color == "purple") {
+            cellArray[portal.index].classList.add('short-portal-purple');
+            existingIndex.push(portal.index);
+        } else {
+            bluePortal.index = null;
+            bluePortal2.index = null;
+
+        }
+    })
 }
 
-
+/* ----------------- CREATION ET POSITIONNEMENT DES TRÉSORS ----------------- */
 let heartIndex = getRandomIndex();
 
 // Ajouter le trésor aléatoirement
@@ -207,30 +285,49 @@ if (treasureIndex != existingIndex.includes(treasureIndex)) {
 }
 
 
+/* ----------------- CREATION ET POSITIONNEMENT DES DRAGONS ----------------- */
 
 // Ma fonction qui me permet de créer plusieurs dragons, je leur donne un index random au départ
-createRandomDragons(100);
-
-console.log(dragons);
-
-
+createRandomDragons(50);
 let dragon = dragons[Math.floor(Math.random() * dragons.length)]
 dragon.isActive = true;
 insertDragon(dragon.index, dragon.className);
 
 
+/* ----------------- CREATION ET POSITIONNEMENT DES ENVIRONMENTS ----------------- */
 
 // Création d'une boucle pour ajouter des rochers aléatoirement
-
-createEnvironment(65, 'rock');
-createEnvironment(30, 'grass');
-
-
+if (totalForWin <= 8 && isMobile != 'true') {
+    createEnvironment(60, 'rock');
+    createEnvironment(30, 'grass');
+} else if (totalForWin <= 8 && isMobile == 'true') {
+    createEnvironment(30, 'rock');
+    createEnvironment(15, 'grass');
+}
+else if (totalForWin > 8 && totalForWin <= 12 && isMobile != 'true') {
+    createEnvironment(50, 'rock');
+    createEnvironment(25, 'grass');
+} else if (totalForWin > 8 && totalForWin <= 12 && isMobile == 'true') {
+    createEnvironment(25, 'rock');
+    createEnvironment(12, 'grass');
+} else if (totalForWin > 12 && isMobile != 'true') {
+    createEnvironment(40, 'rock');
+    createEnvironment(20, 'grass');
+} else if (totalForWin > 12 && isMobile == 'true') {
+    createEnvironment(20, 'rock');
+    createEnvironment(10, 'grass');
+}
 
 
 // Faire fonctionner le déplacement du joueur avec les touches directionnelles.
 let player = document.querySelector('.player');
 playerIndex = cellArray.indexOf(player);
+
+
+
+
+
+
 
 
 // ----------------------------------- FONCTIONS ------------------------------------ //
@@ -259,13 +356,14 @@ function dragonRoarSound() {
     dragonRoar.play();
 }
 
-function calculateRow(portalIndex){
-    return Math.floor(portalIndex / 20);
+function calculateRow(portalIndex, nbrRows) {
+    return Math.floor(portalIndex / nbrRows);
 }
 
-function calculateCol(portalIndex){
-    return portalIndex % 20
+function calculateCol(portalIndex, nbrCols) {
+    return portalIndex % nbrCols
 }
+
 function getRandomIndex() {
     return Math.floor(Math.random() * cellArray.length);
 }
@@ -436,7 +534,7 @@ document.addEventListener('keydown', (event) => {
 
     playerIndex = changePlayerIndex();
 
-    
+
     console.log(playerIndex, purplePortal.index);
     // Si joueurIndex == purplePortal.index, new joueurIndex = purplePortal2.index
     let recentlyTeleported = false;
@@ -451,12 +549,12 @@ document.addEventListener('keydown', (event) => {
             return playerIndex;
         }
     }
-     
+
     teleportation(purplePortal, purplePortal2);
     teleportation(purplePortal2, purplePortal);
     teleportation(bluePortal, bluePortal2);
     teleportation(bluePortal2, bluePortal);
-     
+
     // Index du joueur divisé par 20, ou modulo 20 on arrondit le résultat si division, et cela donne la ligne et la colonne du joueur 
 
     dragons.forEach(dragon => {
@@ -569,12 +667,12 @@ document.querySelector('.gamepad').addEventListener('touchstart', (e) => {
             return playerIndex;
         }
     }
-     
+
     teleportation(purplePortal, purplePortal2);
     teleportation(purplePortal2, purplePortal);
     teleportation(bluePortal, bluePortal2);
     teleportation(bluePortal2, bluePortal);
-   
+
 
     dragons.forEach(dragon => {
         if (dragon.isActive) {
@@ -606,7 +704,8 @@ document.querySelector('.gamepad').addEventListener('touchstart', (e) => {
         dragon.index = insertDragon(dragon.index, dragon.className);
         // insertDragon(Math.random.dragons.index, Math.random.dragons.className);
 
-        if (countTreasure == 3 || countTreasure == 6 || countTreasure == 9 || countTreasure == 11 || countTreasure == 13) {
+
+        if (countTreasure == 3 || countTreasure == 6 || countTreasure == 9 || countTreasure == 11 || countTreasure == 13 || countTreasure == 15 || countTreasure == 17 || countTreasure == 19) {
             while (existingIndex.includes(heartIndex)) { heartIndex = getRandomIndex(); }
             cellArray[heartIndex].classList.add('heart');
             console.log(`Coeur ajouté en ${heartIndex}`);
